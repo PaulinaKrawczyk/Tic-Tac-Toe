8ae47346fefc31f43bf9a153d8a3007c0019fca7
Paulina <paulina.krawczyk@hotmail.com>
2021-04-18 22:22:10 +0200

    stage 4

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,5 +1,6 @@
 package tictactoe;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -17,6 +18,8 @@ public class Main {
 		int sumSecondDiagonal;
 		boolean winX = false;
 		boolean winO = false;
+		int first = 0;
+		int second = 0;
 
 		System.out.println("---------");
 		for (int i = 0; i < string.length(); i++) {
@@ -28,6 +31,7 @@ public class Main {
 		}
 		System.out.println("---------");
 		int[][] asciiArray = new int[3][3];
+		char[][] arr = new char[3][3];
 
 		for (char c : array) {
 			if (c == 'X') {
@@ -39,32 +43,78 @@ public class Main {
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
 				asciiArray[i][j] = array[i * 3 + j];
+				arr[i][j] = array[i * 3 + j];
+
 			}
 		}
 
-		for (int i = 0; i < 3; i++) {
-			sumLeftRight = asciiArray[i][0] + asciiArray[i][1] + asciiArray[i][2];
-			sumTopDown = asciiArray[0][i] + asciiArray[1][i] + asciiArray[2][i];
-			sumDiag = asciiArray[0][0] + asciiArray[1][1] + asciiArray[2][2];
-			sumSecondDiagonal = asciiArray[2][0] + asciiArray[1][1] + asciiArray[0][2];
 
-			if (sumLeftRight == 264 || sumTopDown == 264 || sumDiag == 264 || sumSecondDiagonal == 264) {
-				winX = true;
-			} else if (sumLeftRight == 237 || sumTopDown == 237 || sumDiag == 237 || sumSecondDiagonal == 237) {
-				winO = true;
+//		if (scanner.hasNextInt()) {
+//			first = scanner.nextInt();
+//			second = scanner.nextInt();
+//		} else {
+//			System.out.println("You should enter numbers!");
+//		}
+//		while (true) {
+
+		do {
+			System.out.println("Enter the coordinates:");
+			try {
+				first = scanner.nextInt();
+				second = scanner.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("You should enter numbers!");
+				scanner.nextLine();
+				continue;
 			}
-		}
-		if (winO && winX || Math.abs(sumX - sumO) > 1) {
-			System.out.println("Impossible");
-		} else if (winX) {
-			System.out.println("X wins");
-		} else if (winO) {
-			System.out.println("O wins");
-		} else if (sumX + sumO == 9) {
-			System.out.println("Draw");
-		} else
-			System.out.println("Game not finished");
+//		System.out.println(first);
+//		System.out.println(second);
+
+
+			if (first > 0 && first < 4 && second > 0 && second < 4) {
+				if (arr[first - 1][second - 1] == '_') {
+					arr[first - 1][second - 1] = 'X';
+					System.out.println("---------");
+					for (int i = 0; i < 3; i++) {
+						System.out.print("| ");
+						for (int j = 0; j < 3; j++) {
+							System.out.print(arr[i][j] + " ");
+						}
+						System.out.println("|");
+					}
+					System.out.println("---------");
+					break;
+				} else {
+					System.out.println("This cell is occupied! Choose another one!");
+				}
+			} else {
+				System.out.println("Coordinates should be from 1 to 3!");
+			}
+
+		} while (true);
+//		for (int i = 0; i < 3; i++) {
+//			sumLeftRight = asciiArray[i][0] + asciiArray[i][1] + asciiArray[i][2];
+//			sumTopDown = asciiArray[0][i] + asciiArray[1][i] + asciiArray[2][i];
+//			sumDiag = asciiArray[0][0] + asciiArray[1][1] + asciiArray[2][2];
+//			sumSecondDiagonal = asciiArray[2][0] + asciiArray[1][1] + asciiArray[0][2];
+//
+//			if (sumLeftRight == 264 || sumTopDown == 264 || sumDiag == 264 || sumSecondDiagonal == 264) {
+//				winX = true;
+//			} else if (sumLeftRight == 237 || sumTopDown == 237 || sumDiag == 237 || sumSecondDiagonal == 237) {
+//				winO = true;
+//			}
+//		}
+//		if (winO && winX || Math.abs(sumX - sumO) > 1) {
+//			System.out.println("Impossible");
+//		} else if (winX) {
+//			System.out.println("X wins");
+//		} else if (winO) {
+//			System.out.println("O wins");
+//		} else if (sumX + sumO == 9) {
+//			System.out.println("Draw");
+//		} else
+//			System.out.println("Game not finished");
+//
 	}
-
 }
 
